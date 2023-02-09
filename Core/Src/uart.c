75f5fd120665bbd1290ee3043dc2fdb5b1d65d09
kkyun2e2 <taxi7741@naver.com>
2023-02-09 10:54:30 +0900

    .

---
+++ Core/Src/uart.c
@@ -0,0 +1,16 @@
+/*
+ * uart.c
+ *
+ *  Created on: Feb 3, 2023
+ *      Author: NPEDU001
+ */
+
+#include "uart.h"
+#include <stdio.h>;
+
+extern UART_HandleTypeDef huart1;
+int _write(int file,char *p, int len)
+{
+	HAL_UART_Transmit(&huart1, p, len, 1);
+	return len;
+}
